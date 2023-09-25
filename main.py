dca7cbb34c40f23fe96be7f40b1097014f760216
Sathishsivan <144932593+Sathishsivan@users.noreply.github.com>
2023-09-25 17:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,3 @@ def fact_rec(n):
 number = int(input("enter a value"))
 res = fact_rec(number)
 print("the factroial of {} is {}.".format(number, res))
-
-program2
-
-
-def isLeapyear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
-
-
-year = int(input("enter a year:"))
-if isLeapyear(year):
-  print('{} is a leap year .'.format(year))
-else:
-  print('{} is not a leapyear .'.format(year))
